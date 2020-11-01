c9fbc58ad97c85606b41afa6ad3aa2c38b1630b5
vicgwee <47884004+vicgwee@users.noreply.github.com>
2020-11-02 00:21:12 +0800

    invert image in findCharBds

--- src/findCharBds.m
+++ src/findCharBds.m
@@ -5,20 +5,11 @@ if ndims(img) == 3
     img=rgb2gray(img);
 end
 
-%percentage of fourier descriptors used to recover image
-usedPercent=0.15;
- 
-[rows cols]=size(img);
-%transform gray image to binary image (bwImg).
-%As input image is RGB, resulting binary image may look a little different from
-%original RGB image.
-bwImg=im2bw(img,graythresh(img));
+bwImg=1.-im2bw(img,graythresh(img));
+Bd=bwboundaries(bwImg,8,'noholes'); 
 
-% get the boundaries of binary image bwImg,alternatively, 4--connectivity,
-%'ccw'--counter clockwise.X,Y store the coordinates of ordered boundary
-%points.
-imageDiagonal = sqrt(size(bwImg,1)^2 + size(bwImg,2)^2);
-Bd=bwboundaries(bwImg,8,'holes'); 
+[rows cols]=size(img);
+imageDiagonal = sqrt(rows^2 + cols^2);
 Bd_chars = filterBoundariesByDiagLength(Bd, 30, imageDiagonal- 30);
 
 end
