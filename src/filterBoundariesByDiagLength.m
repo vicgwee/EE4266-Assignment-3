51c7c65c627d81aaf57149b9d28b13b07879b981
vicgwee <47884004+vicgwee@users.noreply.github.com>
2020-11-02 13:31:38 +0800

    change filterBoundariesByDiagLength

--- src/filterBoundariesByDiagLength.m
+++ src/filterBoundariesByDiagLength.m
@@ -1,15 +1,14 @@
-function B_chars = filterBoundariesByDiagLength(B_raw, min_l, max_l)
-%filterBoundariesByDiagLength Returns B_chars, subset of array B_raw with
-%diagonal within min_l and max_l
+function B_chars = filterBoundariesByDiagLength(B_raw, min_diag, max_diag)
+%filterBoundariesByDiagLength Returns B_chars, subset of array B_raw with diagonal within min_diag and max_diag
 %   Detailed explanation goes here
 B_chars = {};
 for i = 1:length(B_raw)
     bounding_pts = B_raw{i};
-    if size(bounding_pts,1) > 5     
+    if size(bounding_pts,1) >= 4     % need at least 4 pixels to form a boundary around 1 pixel
         x = bounding_pts(:,1);
         y = bounding_pts(:,2);
-        diagonal = sqrt((max(x) - min(x))^2 + (max(y) - min(y))^2);
-        if diagonal > min_l && diagonal < max_l
+        diag = sqrt((max(x) - min(x))^2 + (max(y) - min(y))^2);
+        if diag > min_diag && diag < max_diag
             B_chars{end+1, 1} = bounding_pts;
         end
     end
