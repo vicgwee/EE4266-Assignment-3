62561a1653c127ebcb918536c9d1b5031b401b99
vicgwee <47884004+vicgwee@users.noreply.github.com>
2020-11-02 13:30:49 +0800

    add Bd numbers to showBdsOnImg()

--- src/showBdsOnImg.m
+++ src/showBdsOnImg.m
@@ -9,5 +9,10 @@ function showBdsOnImg(Bd, img, txt)
         cidx = mod(k,length(colors))+1;
         plot(boundary(:,2), boundary(:,1),...
             colors(cidx),'LineWidth',2);
+        %randomize text position for better visibility
+        rndRow = ceil(length(boundary)/(mod(rand*k,7)+1));
+        col = boundary(rndRow,2); row = boundary(rndRow,1);
+        h = text(col+1, row-1, num2str(k));
+        set(h,'Color',colors(cidx),'FontSize',16,'FontWeight','bold')
     end
 end
